ed8ac7091c4ae8a173261ce2acc3cf0b5c4e0481
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2022-11-24 11:00:02 +0530

    Added: Day one practice problems

---
+++ StringsEqual.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.dayone;
+	public class StringsEqual {
+		public static void main(String[] args) {
+		String str1="HELLO";
+		String str2="HELLO";
+		if (str1==str2) {
+		System.out.println("String is equal");
+		}else {
+		System.out.println("String is not equal" );
+	} 
+	}
+}
---
+++ Sum.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.dayone;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		int a=20, b=30;
+		int sum=a + b;
+		System.out.println(sum);
+	}
+
+}
